475144c7c7a7a9833c274996f215bffd39903df1
April Auger <april@sitepioneers.com>
2019-08-10 07:36:15 -0700

    Added header.js, .gitignore, and Exercise 1.3

---
+++ week-1/aauger-exercise-1.3.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  auger-exercise-1.3.js
+; Author: April Auger
+; Date:   9 August 2019
+; Description:
+;===========================================
+*/
+
+// Start program
+
+// Required modules
+var url = require('url');
+var header = require("../header.js");
+
+// Output the header to the console
+console.log(header.display("April", "Auger", "Exercise 1.3") + '\n');
+
+// Variable
+var parseUrl = url.parse('https://www.mysite.com/user?name=auger');
+
+// Console log output
+console.log(parseUrl.protocol);
+console.log(parseUrl.host);
+console.log(parseUrl.query);
+
+// End program
