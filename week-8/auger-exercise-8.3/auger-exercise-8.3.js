35c7bf191c9a462ff4ee0176f633e5d363ca5c1e
April Auger <april@sitepioneers.com>
2019-09-29 06:52:00 -0700

    Copies of the EMS app.js, routes.js, and EJS files with Exercise 8.3 updates

---
+++ week-8/auger-exercise-8.3/auger-exercise-8.3.js
@@ -0,0 +1,94 @@
+/*
+============================================
+; Title:  app.js
+; Author: April Auger
+; Date:   29 September 2019
+; Description: Demonstrates how to create an
+;			   employee management system using
+;			   Express, EJS, and MongoDB.
+;===========================================
+*/
+
+// Required modules
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var helmet = require("helmet");
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
+var header = require('../../header.js');
+var routes = require('../routes.js');
+
+// Start program
+
+// Output the header to the console
+console.log(header.display('April', 'Auger', 'Exercise 8.3') + '\n');
+
+// Setup CSRF protection
+var csrfProtection = csrf({ cookie: true });
+
+// Variable storing a new Express application
+var app = express();
+
+// Use statements
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(request, response, next) {
+  var token = request.csrfToken();
+  response.cookie("XSRF-TOKEN", token);
+  response.locals.csrfToken = token;
+  next();
+});
+// Set the Content-Security-Policy in the HTTP Header
+app.use(helmet.xssFilter());
+
+// Database Connection String
+var mongoDB = "mongodb+srv://aauger:wNoz7FuS2dYd2aHb@buwebdev-cluster-1-bzl71.mongodb.net/ems?retryWrites=true&w=majority";
+
+// Connect to database
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// local mongo database
+// mongoose.connect("mongodb://localhost:27017/ems",{useNewUrlParser: true, useUnifiedTopology: true});
+
+// Promise
+mongoose.Promise = global.Promise;
+
+// Variable to hold database connection
+var db = mongoose.connection;
+
+// General error handling
+db.on("error", console.error.bind(console, "MongoDB connected error: "));
+
+// Display message once connected to the database
+db.once("open", function() {
+    console.log("Application connected to MongoDB Atlas");
+});
+
+// The application routes
+app.use(routes);
+
+// Set up a path to the assets directory
+var assets = path.resolve(__dirname, "assets");
+
+// Send static files from the assets directory
+app.use('/assets', express.static(assets));
+
+// Location of views folder
+app.set('views', path.resolve(__dirname, 'views'));
+
+// Use the EJS templating engine
+app.set('view engine', 'ejs');
+
+// Use morgan for advanced logging
+app.use(logger('short'));
+
+// Create a server and listen on port 8080
+http.createServer(app).listen(3000, function(){
+	console.log('Application started on port 3000!');
+});
